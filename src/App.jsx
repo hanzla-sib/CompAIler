ff1fc7111afd713e717f6341d7ef9b966c21724b
Hanzla Sibghat <hanzlasib@gmail.com>
2025-10-26 21:36:04 +0100

    feat: Enhance App component with framer-motion for improved animations and transitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import React from 'react'
 import "./App.css"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { motion, AnimatePresence } from 'framer-motion';
 import Home from './pages/Home';
 import NoPage from './pages/NoPage';
 import { ThemeProvider } from './contexts/ThemeContext';
 
 const App = () => {
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      className="min-h-screen"
+    >
       <ThemeProvider>
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="*" element={<NoPage />} />
-          </Routes>
+          <AnimatePresence mode="wait">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="*" element={<NoPage />} />
+            </Routes>
+          </AnimatePresence>
         </BrowserRouter>
       </ThemeProvider>
-    </>
+    </motion.div>
   )
 }
 
